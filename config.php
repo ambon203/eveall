483baeb81abbc085e1aafb1d5e9b8ac320e8925c
ambon203 <31027420+ambon203@users.noreply.github.com>
2020-01-18 11:38:38 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?
+/*gati bisa gak gati bisa*/
+$user_agent = "Mozilla/5.0 (Linux; Android 7.1.2; Redmi 4A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36";
+
+/*note=
+waktu daftar email pass
+wajip sama */
+$email = "maysandytri@gmail.com";
+$pass = "ambon.203";
+
+
+/*link pass  https://semawur.com/J1CdIDdyLF   */
+$pass_scrip = "2020";
+
+
